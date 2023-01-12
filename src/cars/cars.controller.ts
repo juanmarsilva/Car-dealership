d84e52885ca4aa2292c0ea2062b50a2ee4c581d6
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-01-12 12:49:03 -0300

    create Post, Patch and Delete method

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Patch, Delete } from '@nestjs/common';
+import { Body, Put } from '@nestjs/common/decorators';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -18,6 +19,29 @@ export class CarsController {
     @Get(':id')
     getCarById( @Param('id', ParseIntPipe ) id: number ) {
         return this.carsService.findOneById( id );
+    }
+
+    @Post()
+    createNewCar( @Body() body: any ) {
+        
+    }
+
+
+    @Patch(':id')
+    updateCar(
+        @Body() body: any, 
+        @Param('id', ParseIntPipe ) id: number 
+    ) {
+
+
+        
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe ) id: number ) {
+
+
+       
     }
 
 }
