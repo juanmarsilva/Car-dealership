8543b46cd76b916882ad6ab126d06796644a262c
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-04-25 18:52:04 -0300

    md create brand service with dtos and entity

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    const { name } = createBrandDto;
+
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLowerCase(),
+      createdAt: new Date().getTime(),
+    };
+
+    this.brands.push(brand);
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find((b) => b.id === id);
+    if (!brand) throw new NotFoundException(`Brand with id: ${id} not found`);
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brand = this.findOne(id);
+
+    this.brands = this.brands.map((b) => {
+      if (b.id === id) {
+        brand.updatedAt = new Date().getTime();
+        brand = { ...brand, ...updateBrandDto };
+        return brand;
+      }
+      return b;
+    });
+
+    return brand;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter((brand) => brand.id !== id);
+    return {};
   }
 }
